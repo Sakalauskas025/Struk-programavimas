3fb474e27790faa81ba8d5ee8d3d9b8b7dcaf86d
Sakalauskas025 <vilius.sakalauskas8@gmail.com>
2024-09-12 14:54:46 +0300

    iniciuota pirma programele

---
+++ 1programele/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string vardas = "Vilius";
+    string pavarde;
+    int amzius;
+
+    pavarde = "Sakalauskas";
+    amzius = 18;
+    cout << "Studento vardas: " <<vardas <<endl;
+    cout << "Studento pavarde: " <<pavarde <<endl;
+    cout << "studento amzius: " <<amzius <<endl;
+
+    return 0;
+}
